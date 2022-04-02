4d336c1ea3da3c04aff6abaeaf5e76c37d6b3e6e
Jay Hunter <hunter.jay.k@gmail.com>
2022-04-02 22:58:14 +1000

    Prep for Day 2

---
+++ 2021/Rust/day_2_dive/target/rls/debug/deps/day_2_dive-4a0c4890990d3595.d
@@ -0,0 +1,5 @@
+d:\Documents\Programming\Hobby\Advent of Code\AOC\2021\Rust\day_2_dive\target\rls\debug\deps\day_2_dive-4a0c4890990d3595.rmeta: src\main.rs
+
+d:\Documents\Programming\Hobby\Advent of Code\AOC\2021\Rust\day_2_dive\target\rls\debug\deps\day_2_dive-4a0c4890990d3595.d: src\main.rs
+
+src\main.rs:
---
+++ 2021/Rust/day_2_dive/target/rls/debug/deps/day_2_dive-60af8855c6d9eb36.d
@@ -0,0 +1,5 @@
+d:\Documents\Programming\Hobby\Advent of Code\AOC\2021\Rust\day_2_dive\target\rls\debug\deps\day_2_dive-60af8855c6d9eb36.rmeta: src\main.rs
+
+d:\Documents\Programming\Hobby\Advent of Code\AOC\2021\Rust\day_2_dive\target\rls\debug\deps\day_2_dive-60af8855c6d9eb36.d: src\main.rs
+
+src\main.rs:
